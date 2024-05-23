8e4c09d280d96fa9585a95013ba87da25014d8df
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-05-23 16:57:03 -0500

    Feat: 23-05-2024 - Consumo de servicio con HTTPClient.
    Consumo de servicio de Spring Boot con uso de HTTPClient.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneralService } from './api/general.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'appcwds220241';
+	title = 'appcwds220241';
+
+	constructor(
+		private generalService: GeneralService
+	) {
+
+	}
+
+	ngOnInit(): void {
+		this.generalService.indexGet().subscribe({
+			next: (response: any) => {
+				console.log(response);
+			},
+			error: (error: any) => {
+				console.log(error);
+			}
+		});
+	}
 }
